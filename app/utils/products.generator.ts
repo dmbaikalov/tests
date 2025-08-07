0a935d5eb9a24c815ca8384d3a7303f0d94938b3
dmbaikalov <dmbaikalov@users.noreply.github.com>
2025-08-07 12:42:51 +0000

    deploy: 75bf0083f13c362e30bdf6db2619c8e136a8150f

--- app/utils/products.generator.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export function getProductData() {
-  return {
-    title: faker.commerce.productName(),
-    description: faker.commerce.productDescription(),
-    price: parseFloat(faker.commerce.price({ min: 1, max: 80})),
-  };
-}
--- app/utils/user.id.generator.ts
+++
@@ -1,5 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export function getRandomUserId() {
-  return faker.number.int({ min: 1, max: 30 });
-}
